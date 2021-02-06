2966dc7ea32c44853e60ed55e50e7e525959759b
richiejoel <richyard558@outlook.es>
2021-02-06 12:35:44 -0500

    Elements dynamics Logopeda

--- src/components/PaletteInteractive/PaletteInteractive.tsx
+++ src/components/PaletteInteractive/PaletteInteractive.tsx
@@ -1,9 +1,93 @@
 import React from "react";
+import {connect, useSelector} from "react-redux";
+import AddCard from "../../assets/svg/add_note.svg";
+import Draggable from "react-draggable";
+import "./PaletteInteractive.scss";
+import "../../styles/theme.scss";
 
-export default function PaletteInteractive(): JSX.Element {
+
+function PaletteInteractive(props): JSX.Element {
+  const {item, items,updatePos, deleteNote, newitem, setItem, keyPress } = props;
+  const theme_global_color = useSelector((state: any) => state.theme_global);
+  const fix = () => {
+    setItem(`Lesson`);
+    newitem();
+  }
+  //{`${item.item}`}
   return (
-    <div className="palette-interactive">
-      <h2>Hola</h2>
+    <div className={`palette-interactive ${theme_global_color.theme}`}>
+      <div className="palette-interactive__content">
+      <div className="palette-interactive__left">
+        <div className="notes" 
+      
+        onClick={()=>{fix()}}
+       
+        >
+        <img id="img-add" src={AddCard} alt="img-add"/>
+        <span>Añadir elemento</span>
+        </div>
+        <div className="notes">
+        <img id="img-add" src={AddCard} alt="img-add"/>
+        <span>Añadir texto</span>
+        </div>
+        
+      </div>
+      <div className="palette-interactive__right">
+        <div className="palette-interactive__right__colors">
+        </div>
+        <div className="palette-interactive__right__colors">
+        </div>
+        <div className="palette-interactive__right__colors">
+        </div>
+        <div className="palette-interactive__right__colors">
+        </div>
+        <div className="palette-interactive__right__colors">
+        </div>
+        <div className="palette-interactive__right_colors">
+        </div>
+        <div className="palette-interactive__right__colors">
+        </div>
+        <div className="palette-interactive__right__colors">
+        </div>
+      </div>
+
+    <div>
+    <div id="new-item">
+        {/*<input
+          value={item}
+          onChange={(e) => setItem("Lesson")}
+          placeholder="Enter something..."
+          onKeyPress={(e) => keyPress(e)}
+        />
+        <button onClick={newitem}>ENTER</button>*/}
+      </div>
+      {items.map((item, index) => {
+        return (
+          <Draggable
+            key={item.id}
+            defaultPosition={item.defaultPos}
+            onStop={(e, data) => {
+              updatePos(data, index);
+            }}
+          >
+            <div style={{ backgroundColor: item.color }} className="box">
+              <input id="input-card" type="text" 
+              
+              onChange={(e) => setItem(e.target.value)}
+              placeholder="Title option..."
+              
+               />
+              <button id="delete" onClick={(e) => deleteNote(item.id)}>
+                X
+              </button>
+            </div>
+          </Draggable>
+        );
+      })}
+    </div>
+      </div>
     </div>
   );
 }
+
+export default connect()(PaletteInteractive);
--- src/pages/CreateLessonCard/CreateLessonCard.tsx
+++ src/pages/CreateLessonCard/CreateLessonCard.tsx
@@ -1,9 +1,14 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { connect, useSelector } from "react-redux";
+import { Button} from "semantic-ui-react";
 import Draggable from "react-draggable";
 import { useDropzone } from "react-dropzone";
+import { v4 as uuidv4 } from "uuid";
+import randomColor from "randomcolor";
+//var randomColor = require("randomcolor");
 
 import CardPreview from "../../components/CardPreview";
+import PaletteInteractive from "../../components/PaletteInteractive";
 
 
 import "./CreateLessonCard.scss";
@@ -37,13 +42,94 @@ function CreateLessonCard() {
 
   const dragHandlers = { onStart: onStart, onStop: onStop };
 
+  const [item, setItem] = useState("Lesson");
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("items")!) || []
+  );
+
+  const newitem = () => {
+        //if (item.trim() !== "") {
+      const newitem = {
+        id: uuidv4(),
+        item: item,
+        color: randomColor({
+          luminosity: "light",
+        }),
+        defaultPos: { x: 100, y: -60 },
+      };
+      setItems((items) => [...items, newitem]);
+      //setItem("");
+    /*} /*
+    else {
+      alert("Enter a item");
+      setItem("");
+    }*/
+  };
+
+  const keyPress = (event) => {
+    var code = event.keyCode || event.which;
+    if (code === 13) {
+      newitem();
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  const updatePos = (data, index) => {
+    let newArr = [...items];
+    newArr[index].defaultPos = { x: data.x, y: data.y };
+    setItems(newArr);
+  };
+
+  const deleteNote = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
   return (
     <div className={`create-lesson-card ${theme_global.theme}`}>
-      <Draggable {...dragHandlers}>
-      <div className="box">
-      <CardPreview/>
-        </div>
-      </Draggable>
+      <div className="content-lesson">
+        {/*<Draggable {...dragHandlers}>
+          <div className="box">
+            <CardPreview/>
+          </div>
+  </Draggable>*/}
+      <Button className="btn-lesson" >
+        Guardar Lección
+      </Button>
+      </div>
+      <div className="palette">
+        <PaletteInteractive item={item} items={items} updatePos={updatePos} 
+        deleteNote={deleteNote} newitem={newitem} setItem={setItem} keyPress={keyPress} />
+        {/*<div id="new-item">
+        <input
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          placeholder="Enter something..."
+          onKeyPress={(e) => keyPress(e)}
+        />
+        <button onClick={newitem}>ENTER</button>
+      </div>
+      {items.map((item, index) => {
+        return (
+          <Draggable
+            key={item.id}
+            defaultPosition={item.defaultPos}
+            onStop={(e, data) => {
+              updatePos(data, index);
+            }}
+          >
+            <div style={{ backgroundColor: item.color }} className="box">
+              {`${item.item}`}
+              <button id="delete" onClick={(e) => deleteNote(item.id)}>
+                X
+              </button>
+            </div>
+          </Draggable>
+        );
+      })}*/}
+      </div>
     </div>
   );
 }
